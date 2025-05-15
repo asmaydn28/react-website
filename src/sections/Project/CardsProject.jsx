f79127c68e655c82bbda76220be07a22d19f7570
ASIM AYDIN <133012227+asmaydn28@users.noreply.github.com>
2025-05-15 17:49:36 +0300

    update

--- src/sections/Project/CardsProject.jsx
+++ src/sections/Project/CardsProject.jsx
@@ -6,7 +6,7 @@ function CardsProject() {
       <ProjectCard
         title={"React ToDo App"}
         text={"React ile oluşturulmuş basit bir yapılacaklar listesi uygulaması."}
-        img={"/public/img/15.png"}
+        img={"/img/15.png"}
         github={"https://github.com/asmaydn28/react-todo-app"}
         netlify={"https://asimaydin15.netlify.app/"}
       />
@@ -14,7 +14,7 @@ function CardsProject() {
       <ProjectCard
         title={"First React Project"}
         text={"React ile oluşturulmuş İlk projem."}
-        img={"/public/img/14.png"}
+        img={"/img/14.png"}
         github={"https://github.com/asmaydn28/first-react-project"}
         netlify={"https://asimaydin14.netlify.app/"}
       />
