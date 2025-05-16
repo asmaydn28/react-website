95b9960e6a734c1150f666122e5c416a82d0d333
ASIM AYDIN <133012227+asmaydn28@users.noreply.github.com>
2025-05-16 16:10:01 +0300

    img card cardsproject

--- src/sections/Project/Card.jsx
+++ src/sections/Project/Card.jsx
@@ -1,22 +1,29 @@
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button } from "react-bootstrap";
 
 const ProjectCard = ({ title, text, img, github, netlify }) => {
   return (
     <Card className="m-4 rounded-0">
-        <Card.Title className='p-2 fw-semibold'>{title}</Card.Title>
-        <Card.Text className='ps-2 pb-2'>{text}</Card.Text>
-        <Card.Img className='rounded-0' variant="top" src={img} />
-      <Card.Body className='bg-secondary-subtle p-0'>
-        <Button variant="info" className="w-50 rounded-0 fw-semibold">
-          <a href={github} target="_blank" rel="noopener noreferrer" className="text-decoration-none text-white">
-            Github
-          </a>
-        </Button>
-        <Button variant="danger" className='w-50 rounded-0 fw-semibold'>
-          <a href={netlify} target="_blank" rel="noopener noreferrer" className="text-decoration-none text-white">
-            Canlı
-          </a>
-        </Button>
+      <Card.Title className="p-2 fw-semibold">{title}</Card.Title>
+      <Card.Text className="ps-2 pb-2">{text}</Card.Text>
+      <Card.Img className="rounded-0" variant="top" src={img} />
+      <Card.Body className="bg-secondary-subtle p-0">
+        <a
+          href={github}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="btn btn-primary text-decoration-none text-white w-50 rounded-0 fw-semibold"
+        >
+          Github
+        </a>
+
+        <a
+          href={netlify}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="btn btn-danger text-decoration-none text-white w-50 rounded-0 fw-semibold"
+        >
+          Canlı
+        </a>
       </Card.Body>
     </Card>
   );
